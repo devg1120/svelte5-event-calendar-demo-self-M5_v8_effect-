b080e194da90371e52df82064d4ca7a4b268dbec
root <root@ubuntu-server-01.myguest.virtualbox.org>
2025-04-02 16:42:33 +0900

    first

---
+++ src/lib/packages/day-grid/src/EventSpace.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+
+    import {
+        height,
+    } from "@event-calendar/core";
+
+   //console.log("space:", height());
+   let spaceHeight = 26;
+
+</script>
+
+<style>
+.eventSpace {
+  height : var(--SpaceHeight);
+  }
+</style>
+
+<div 
+  class="eventSpace" 
+  style="--SpaceHeight: {spaceHeight}px"
+
+></div>
