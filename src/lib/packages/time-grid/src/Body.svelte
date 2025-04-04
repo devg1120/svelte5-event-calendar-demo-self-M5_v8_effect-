fec6f6fcdad3c7a7082e6be9ffda0288eba3caff
root <root@ubuntu-server-01.myguest.virtualbox.org>
2025-04-04 17:22:42 +0900

    up

--- src/lib/packages/time-grid/src/Body.svelte
+++ src/lib/packages/time-grid/src/Body.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+   import { untrack } from "svelte";
     import { getContext } from "svelte";
     import Section from "./Section.svelte";
     interface Props {
@@ -13,6 +14,7 @@
     let el = $state();
     let compact = $state();
     let lines = $state([]);
+    //let lines = $state(new Array(49));
 
     function scrollToTime() {
         el.scrollTop =
@@ -21,10 +23,19 @@
     $effect(() => {
         $_bodyEl = el;
     });
+
+
     $effect(() => {
         compact = $slotDuration.seconds >= 3600;
         lines.length = $_times.length;
+        console.log("lines.length",lines.length)
     });
+
+    //let  lines = $state([$_times.length]);
+    //let  lines = $state([49]);
+    //let lines = $derived.by(() => { return new Array($_times.length);} )
+
+    
     $effect(() => {
         if (el) {
             $_viewDates;
@@ -46,3 +57,36 @@
         </Section>
     </div>
 </div>
+
+<!--
+<div bind:this={el} class="{$theme.body}{compact ? ' ' + $theme.compact : ''}">
+    <div class={$theme.content}>
+        <Section>
+            {#snippet lines()}
+                {#each lines as line}
+                    <div class={$theme.line}></div>
+                {/each}
+            {/snippet}
+            {@render children?.()}
+        </Section>
+    </div>
+</div>
+-->
+
+<!--
+<div
+    bind:this={el}
+    class="{$theme.body}{compact ? ' ' + $theme.compact : ''}"
+>
+    <div class="{$theme.content}">
+        <Section>
+            <svelte:fragment slot="lines">
+                {#each lines as line}
+                    <div class="{$theme.line}"></div>
+                {/each}
+            </svelte:fragment>
+            <slot></slot>
+        </Section>
+    </div>
+</div>
+-->
